1eb1fda08aaad10e446b9bc2abbddf894b1b7a99
Keane Quibilan <arv.r2vq@gmail.com>
2021-09-01 14:54:40 -0400

    Format and organize gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,21 +47,21 @@ android {
 
 dependencies {
     // Android Core
-    implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("com.google.android.material:material:1.4.0")
 
     // Dependency Injection
     implementation("io.insert-koin:koin-android:3.1.2")
 
     // Network
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
 
     // Testing
