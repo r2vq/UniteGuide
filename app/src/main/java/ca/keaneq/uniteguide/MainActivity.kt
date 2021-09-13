714a25ef250868914bd61ab11045c3b43037fcaf
Keane Quibilan <arv.r2vq@gmail.com>
2021-09-12 22:19:03 -0400

    Remove bottom nav bar

--- app/src/main/java/ca/keaneq/uniteguide/MainActivity.kt
+++ app/src/main/java/ca/keaneq/uniteguide/MainActivity.kt
@@ -3,12 +3,7 @@ package ca.keaneq.uniteguide
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import ca.keaneq.uniteguide.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,19 +14,6 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_pokemon_list, R.id.navigation_dashboard, R.id.navigation_home
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean =
