e33b8897b7336154a9a231d212603500816bb7bd
Keane Quibilan <arv.r2vq@gmail.com>
2021-10-29 11:57:19 -0400

    Update theme typography

--- app/src/main/java/ca/keaneq/uniteguide/presentation/main/UniteGuideTheme.kt
+++ app/src/main/java/ca/keaneq/uniteguide/presentation/main/UniteGuideTheme.kt
@@ -2,10 +2,13 @@ package ca.keaneq.uniteguide.presentation.main
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun UniteGuideTheme(
@@ -14,9 +17,22 @@ fun UniteGuideTheme(
 ) {
     MaterialTheme(
         colors = if (isDarkMode) Colors.DarkColors else Colors.LightColors,
-        content = content
+        content = content,
+        typography = typography,
     )
 }
+
+private val typography = Typography(
+    h1 = TextStyle(
+        fontSize = 16.sp,
+    ),
+    subtitle1 = TextStyle(
+        fontSize = 14.sp,
+    ),
+    body1 = TextStyle(
+        fontSize = 12.sp,
+    ),
+)
 
 private object Colors {
     val BLACK = Color(0xFF000000)
@@ -29,8 +45,9 @@ private object Colors {
     val DEEP_PURPLE_A700 = Color(0xFF5427A0)
     val DEEP_PURPLE_A700_DARK = Color(0XFF0A00B6)
     val GRAY_800 = Color(0xFF424242)
-    val GRAY_200 = Color(0xFF111111)
+    val GRAY_900 = Color(0xFF111111)
     val BLUE_GRAY_200 = Color(0xFFB0BEC5)
+    val GRAY_50 = Color(0xFFEBEBEB)
 
     val DarkColors = darkColors(
         primary = DEEP_PURPLE_A700,
@@ -41,7 +58,7 @@ private object Colors {
         onSecondary = BLACK,
         background = BLACK,
         onBackground = WHITE,
-        surface = GRAY_200,
+        surface = GRAY_900,
         onSurface = WHITE,
     )
     val LightColors = lightColors(
@@ -51,9 +68,9 @@ private object Colors {
         secondary = ORANGE_700,
         secondaryVariant = ORANGE_700,
         onSecondary = BLACK,
-        background = WHITE,
+        background = GRAY_50,
         onBackground = BLACK,
-        surface = BLUE_GRAY_200,
+        surface = WHITE,
         onSurface = BLACK,
     )
 }
