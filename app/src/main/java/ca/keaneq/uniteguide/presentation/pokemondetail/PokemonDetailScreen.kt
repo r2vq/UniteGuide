e967d325a774400376cd23ea3172c5903b8714d1
Keane Quibilan <arv.r2vq@gmail.com>
2021-11-04 19:08:13 -0400

    Setup scaffold for Pokemon Detail Bottom Sheet

--- app/src/main/java/ca/keaneq/uniteguide/presentation/pokemondetail/PokemonDetailScreen.kt
+++ app/src/main/java/ca/keaneq/uniteguide/presentation/pokemondetail/PokemonDetailScreen.kt
@@ -1,29 +1,45 @@
 package ca.keaneq.uniteguide.presentation.pokemondetail
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ca.keaneq.uniteguide.presentation.pokemondetail.component.PokemonImage
 import ca.keaneq.uniteguide.presentation.pokemondetail.component.PokemonPillRow
 import ca.keaneq.uniteguide.presentation.pokemondetail.component.PokemonStats
 import ca.keaneq.uniteguide.presentation.pokemondetail.viewmodel.PokemonDetailViewModel
 
+@ExperimentalMaterialApi
 @Composable
 fun PokemonDetailScreen(
     viewModel: PokemonDetailViewModel = hiltViewModel(),
 ) {
     val state = viewModel.state.value
     val pokemon = state.pokemon
+    val scope = rememberCoroutineScope()
+    val bottomSheetState = rememberBottomSheetState(initialValue = BottomSheetValue.Collapsed)
+    val scaffoldState = rememberBottomSheetScaffoldState(bottomSheetState = bottomSheetState)
     when {
         pokemon != null -> {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
+            BottomSheetScaffold(
+                scaffoldState = scaffoldState,
+                sheetContent = {
+                    Text(text = "Hello world")
+                },
+                sheetPeekHeight = 0.dp,
+                sheetShape = CutCornerShape(topEnd = 40.dp),
             ) {
-                PokemonImage(pokemon = pokemon)
-                PokemonPillRow(pokemon = pokemon)
-                PokemonStats(pokemonItem = pokemon)
+                LazyColumn(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    item { PokemonImage(pokemon = pokemon) }
+                    item { PokemonPillRow(pokemon = pokemon) }
+                    item { PokemonStats(pokemonItem = pokemon) }
+                }
             }
         }
         state.isLoading -> {
