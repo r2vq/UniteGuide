5dd176b7712881a6f1b1256902f9b2b843a5ef7c
Keane Quibilan <arv.r2vq@gmail.com>
2021-11-09 09:42:57 -0500

    Move compose app out of activity

--- app/src/main/java/ca/keaneq/uniteguide/MainActivity.kt
+++ app/src/main/java/ca/keaneq/uniteguide/MainActivity.kt
@@ -3,19 +3,8 @@ package ca.keaneq.uniteguide
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.rememberNavController
-import ca.keaneq.presentation.main.ActionBar
-import ca.keaneq.presentation.main.NavigationDrawer
-import ca.keaneq.presentation.main.UniteGuideTheme
-import ca.keaneq.presentation.navigation.Navigation
-import ca.keaneq.presentation.navigation.ToolbarState
+import ca.keaneq.presentation.UniteGuide
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,40 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-            val scope = rememberCoroutineScope()
-            val toolbarState = mutableStateOf(ToolbarState.Menu)
-            UniteGuideTheme {
-                val scaffoldState = rememberScaffoldState()
-                Scaffold(
-                    topBar = {
-                        ActionBar(
-                            text = stringResource(id = R.string.app_name),
-                            drawerState = scaffoldState.drawerState,
-                            scope = scope,
-                            toolbarState = toolbarState.value,
-                            navController = navController,
-                        )
-                    },
-                    drawerContent = {
-                        NavigationDrawer(
-                            navController = navController,
-                            drawerState = scaffoldState.drawerState,
-                            scope = scope,
-                        )
-                    },
-                    drawerShape = RoundedCornerShape(topEnd = 24.dp),
-                    content = {
-                        Navigation(
-                            navController = navController,
-                            versionName = BuildConfig.VERSION_NAME,
-                        ) { toolbar ->
-                            toolbarState.value = toolbar
-                        }
-                    },
-                    scaffoldState = scaffoldState,
-                )
-            }
+            UniteGuide(
+                appName = stringResource(R.string.app_name),
+                versionName = BuildConfig.VERSION_NAME
+            )
         }
     }
 }
---
+++ presentation/src/main/java/ca/keaneq/presentation/UniteGuide.kt
@@ -0,0 +1,58 @@
+package ca.keaneq.presentation
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import ca.keaneq.presentation.main.ActionBar
+import ca.keaneq.presentation.main.NavigationDrawer
+import ca.keaneq.presentation.main.UniteGuideTheme
+import ca.keaneq.presentation.navigation.Navigation
+import ca.keaneq.presentation.navigation.ToolbarState
+
+@Composable
+fun UniteGuide(
+    appName: String,
+    versionName: String
+) {
+    val navController = rememberNavController()
+    val scope = rememberCoroutineScope()
+    val toolbarState = remember { mutableStateOf(ToolbarState.Menu) }
+    val scaffoldState = rememberScaffoldState()
+
+    UniteGuideTheme {
+        Scaffold(
+            topBar = {
+                ActionBar(
+                    text = appName,
+                    drawerState = scaffoldState.drawerState,
+                    scope = scope,
+                    toolbarState = toolbarState.value,
+                    navController = navController,
+                )
+            },
+            drawerContent = {
+                NavigationDrawer(
+                    navController = navController,
+                    drawerState = scaffoldState.drawerState,
+                    scope = scope,
+                )
+            },
+            drawerShape = RoundedCornerShape(topEnd = 24.dp),
+            content = {
+                Navigation(
+                    navController = navController,
+                    versionName = versionName,
+                ) { toolbar ->
+                    toolbarState.value = toolbar
+                }
+            },
+            scaffoldState = scaffoldState,
+        )
+    }
+}
