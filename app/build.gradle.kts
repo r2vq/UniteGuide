6118d7894594a9c639503ca97841351858548050
Keane Quibilan <arv.r2vq@gmail.com>
2021-11-12 21:59:22 -0500

    Bump version to v2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ca.keaneq.uniteguide"
         minSdk = 30
         targetSdk = 30
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
